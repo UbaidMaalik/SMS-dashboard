5d4122d68b27c4cb4270b700e768b2bd8d7fd3cc
innova_care <shahab.uddin@f3technologies.eu>
2023-05-27 00:42:58 +0500

    login page updated

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,34 +1,18 @@
-// import { useState } from "react";
-import { Image } from "react-bootstrap";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import "./App.css";
+
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import { Button, Checkbox, Form, Input } from "antd";
-// import { useState } from "react";
+
 
 const Login = () => {
   const onFinish = (values) => {
     console.log("Received values of form: ", values);
   };
 
-  // const [email, setEmail] = useState("");
-  // const [password, setPassword] = useState("");
-
-  // const handleEmailChange = (e) => {
-  //   setEmail(e.target.value);
-  // };
-
-  // const handlePasswordChange = (e) => {
-  //   setPassword(e.target.value);
-  // };
-
-  // const onSubmit = (e) => {
-  //   e.preventDefault();
+ 
   // };
   return (
     <>
-      <Container>
+      {/* <Container>
         <Row className="main-login">
           <Col className="login-left" md={7} sm={7} lg={7}>
             <Row className="justify-content-center form">
@@ -103,7 +87,80 @@ const Login = () => {
             <Image src="./images/Ellipse 7.png" className="ellipse3" />
           </Col>
         </Row>
-      </Container>
+      </Container> */}
+      <div className='login-main-container'>
+        <div className='login-container'>
+            <h1 style={{textAlign:'center'}} >log In</h1>
+            <div>
+  <label className='label'>Username/Email</label>
+        <Input className='input' placeholder='Enter your username' />
+            </div>
+        <div className='mt-15'>
+
+  <label className='label'>Password</label>
+        <Input className='input ' placeholder='Enter your passsword' />
+        </div>
+        <div className='login-button-container'>
+        <Button >Login</Button>
+        </div>
+        <div>
+           <p className='forget-password'>Forget Password?</p>
+        </div>
+       {/* <Form
+      name="normal_login"
+      className="login-form"
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+    >
+      <Form.Item
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your Username!',
+          },
+        ]}
+      >
+        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username"  className='input'/>
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your Password!',
+          },
+        ]}
+      >
+        <Input
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          type="password"
+          placeholder="Password"
+        />
+      </Form.Item>
+      <Form.Item>
+        <Form.Item name="remember" valuePropName="checked" noStyle>
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <a className="login-form-forgot" href="">
+          Forgot password
+        </a>
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit" className="login-form-button">
+          Log in
+        </Button>
+        Or <a href="">register now!</a>
+      </Form.Item>
+    </Form> */}
+    
+   
+        </div>
+      </div>
     </>
   );
 };
