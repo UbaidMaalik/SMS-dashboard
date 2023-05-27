3f0a479c2f7700fa4e4a94b2405ebc2adec52fdc
innova_care <shahab.uddin@f3technologies.eu>
2023-05-27 16:57:12 +0100

    Common component apply

--- src/Entry.js
+++ src/Entry.js
@@ -2,13 +2,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Layout from "./pages/Layout";
+import DashBoard from "./pages/Dashboard/DashBoard";
 
 function Entry() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<DashBoard />} />
           <Route path="/layout" element={<Layout />} />
         </Routes>
       </BrowserRouter>
