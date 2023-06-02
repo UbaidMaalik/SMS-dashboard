636b34fe998691370449b133996959f00be5d9a6
Ubaid Maalik <umaalik45@gmail.com>
2023-06-02 20:55:23 +0500

    Search Filter on fees table

--- src/pages/Dashboard/FeesDataTable.js
+++ src/pages/Dashboard/FeesDataTable.js
@@ -1,6 +1,34 @@
-import { Radio, Table, Modal } from "antd";
-import { useState } from "react";
-
+import React from "react";
+import { SearchOutlined } from "@ant-design/icons";
+import { Radio, Button, Input, Modal, Space, Table } from "antd";
+import { useRef, useState } from "react";
+// import Highlighter from "react-highlight-words";
+const data = [
+  {
+    key: "1",
+    name: "John Brown",
+    age: 32,
+    address: "New York No. 1 Lake Park",
+  },
+  {
+    key: "2",
+    name: "Joe Black",
+    age: 42,
+    address: "London No. 1 Lake Park",
+  },
+  {
+    key: "3",
+    name: "Jim Green",
+    age: 32,
+    address: "Sydney No. 1 Lake Park",
+  },
+  {
+    key: "4",
+    name: "Jim Red",
+    age: 32,
+    address: "London No. 2 Lake Park",
+  },
+];
 const FeesDataTable = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const showModal = () => {
@@ -12,26 +40,123 @@ const FeesDataTable = () => {
   const handleCancel = () => {
     setIsModalOpen(false);
   };
+  const [searchText, setSearchText] = useState("");
+  const [searchedColumn, setSearchedColumn] = useState("");
+  const searchInput = useRef(null);
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText("");
+  };
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+      close,
+    }) => (
+      <div
+        style={{
+          padding: 8,
+        }}
+        onKeyDown={(e) => e.stopPropagation()}
+      >
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{
+            marginBottom: 8,
+            display: "block",
+          }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Search
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close();
+            }}
+          >
+            X
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined
+        style={{
+          color: filtered ? "#1677ff" : undefined,
+        }}
+      />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    // render: (text) =>
+    //   searchedColumn === dataIndex ? (
+    //     <Highlighter
+    //       highlightStyle={{
+    //         backgroundColor: "#ffc069",
+    //         padding: 0,
+    //       }}
+    //       searchWords={[searchText]}
+    //       autoEscape
+    //       textToHighlight={text ? text.toString() : ""}
+    //     />
+    //   ) : (
+    //     text
+    //   ),
+  });
   const columns = [
     {
-      title: "Student's Name",
+      title: "Name",
       dataIndex: "name",
       key: "name",
+      width: "10%",
+      ...getColumnSearchProps("name"),
     },
     {
-      title: "Father's Name",
+      title: "Age",
       dataIndex: "age",
       key: "age",
+      width: "10%",
     },
     {
-      title: "Registration No",
-      dataIndex: "age",
-      key: "age",
+      title: "Address",
+      dataIndex: "address",
+      key: "address",
+      width: "20%",
     },
     {
       title: "Email",
       dataIndex: "address",
       key: "address",
+      width: "20%",
     },
     {
       title: "CNIC",
@@ -61,29 +186,7 @@ const FeesDataTable = () => {
       ),
     },
   ];
-  const data = [
-    {
-      key: "1",
-      name: "John Brown",
-      age: "John Doe",
-      address: "New York No. 1 Lake Park",
-      tags: ["nice", "developer"],
-    },
-    {
-      key: "2",
-      name: "Jim Green",
-      age: 42,
-      address: "London No. 1 Lake Park",
-      tags: ["loser"],
-    },
-    {
-      key: "3",
-      name: "Joe Black",
-      age: 32,
-      address: "Sydney No. 1 Lake Park",
-      tags: ["cool", "teacher"],
-    },
-  ];
+
   return (
     <>
       <div className="container">
@@ -98,7 +201,7 @@ const FeesDataTable = () => {
             onOk={handleOk}
             onCancel={handleCancel}
           >
-            <table class="table caption-top">
+            {/* <table class="table caption-top">
               <thead>
                 <tr>
                   <th scope="col">#</th>
@@ -130,7 +233,8 @@ const FeesDataTable = () => {
                   </td>
                 </tr>
               </tbody>
-            </table>
+            </table> */}
+            <Table columns={columns} dataSource={data} />
           </Modal>
         </div>
       </div>
